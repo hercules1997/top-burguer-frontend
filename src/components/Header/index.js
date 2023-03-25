0b825d2031027c1a8c35fbd72ba7ae81a0d0d051
hercules1997 <hercules.chaves.andrade@gmail.com>
2023-03-25 01:58:40 -0300

    Criando navegação do Menu de navegação

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,14 +1,42 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 import iconCart from '../../assets/iconCart.png'
 import person from '../../assets/person.png'
-import { Container } from './style'
+import {
+  Container,
+  ContainerLeft,
+  PageLinkExit,
+  ContainerRight,
+  ContainerText
+} from './style'
 
 export function Header () {
   return (
     <Container>
-      <img src={iconCart} alt='Ícone de carrinho'/>
-      <img src={person} alt='ícone de person' />
+      <ContainerLeft>
+        <Link className="pageLink" to="/">
+          Home
+        </Link>
+        <Link className="pageLink" to="/produtos">
+          Ver Produtos
+        </Link>
+      </ContainerLeft>
+
+      <ContainerRight>
+        <Link className="pageLink" to="/carrinho">
+          <img src={iconCart} alt="Ícone de carrinho" />
+        </Link>
+        <div className="traco"></div>
+        <Link className="pageLink">
+          <img src={person} alt="logo pessoa" />
+        </Link>
+
+        <ContainerText>
+          <p>Olá, Hércules</p>
+          <PageLinkExit>Sair</PageLinkExit>
+        </ContainerText>
+      </ContainerRight>
     </Container>
   )
 }
