5ee00e29cd478823e3d3f759ca81e7bb5bd2b15f
hercules1997 <hercules.chaves.andrade@gmail.com>
2023-03-27 13:57:54 -0300

    Atualizando configurações de estilo

--- src/containers/Cart/style.js
+++ src/containers/Cart/style.js
@@ -1,22 +1,20 @@
 import styled from 'styled-components'
 
-export const Container = styled.div`
-  background: #2f2f2f;
-align-items: center;
-text-align: center;
-  min-height: 100vh;
-  Button {
-    width: 100%;
-    margin-top: 5px;
-  }
+import ImgLogoCart from '../../assets/imgLogoCart.png'
 
+export const Container = styled.div`
+  background-image: url('${ImgLogoCart}');
+  background: #2f2f2f;
+  min-height: 100vh;
+  display: flex;
+  flex-direction: column;
 `
+
 export const HomeImage = styled.img`
-  width: 100%;
-  height: 250px;
   box-shadow: 0px 12px 35px black;
 `
 export const Wrapper = styled.div`
   display: flex;
+
   justify-content: space-evenly;
 `
