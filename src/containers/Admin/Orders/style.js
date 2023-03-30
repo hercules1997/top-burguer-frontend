1f37e958c0d95af78686fd669e286a5b26d25dd2
hercules1997 <hercules.chaves.andrade@gmail.com>
2023-03-30 16:03:34 -0300

    Aplicando estilos

---
+++ src/containers/Admin/Orders/style.js
@@ -0,0 +1,20 @@
+import styled from 'styled-components'
+
+export const Container = styled.div`
+  background: #121212;
+  min-height: 100vh;
+  padding: 80px;
+
+  /* .tbory {
+    background: black ;
+    color: white ;
+  } */
+`
+export const ProductsImg = styled.img`
+  width: 80px;
+  height: 80px;
+  border-radius: 12px;
+`
+export const TableBody = styled.tbody`
+  background: black;
+`
