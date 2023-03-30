fed1ab24605115d0a6e4a600429b92409fbdcdd0
hercules1997 <hercules.chaves.andrade@gmail.com>
2023-03-30 16:02:46 -0300

    Adicionando a estrutura de tabela com o mui

---
+++ src/containers/Admin/Orders/index.js
@@ -0,0 +1,68 @@
+import Paper from '@mui/material/Paper'
+import Table from '@mui/material/Table'
+import TableBody from '@mui/material/TableBody'
+import TableCell from '@mui/material/TableCell'
+import TableContainer from '@mui/material/TableContainer'
+import TableHead from '@mui/material/TableHead'
+import TableRow from '@mui/material/TableRow'
+import React, { useEffect, useState } from 'react'
+
+import apiTopBurger from '../../../services/api'
+import formatDate from '../../../utils/formatDate'
+import Row from './row'
+import { Container } from './style'
+
+function Orders () {
+  const [orders, setOrders] = useState([])
+  const [rows, setRows] = useState([])
+
+  console.log(orders)
+  useEffect(() => {
+    async function loadOrders () {
+      const { data } = await apiTopBurger.get('orders')
+
+      setOrders(data)
+    }
+
+    loadOrders()
+  }, [])
+
+  function createData (order) {
+    return {
+      name: order.user.name,
+      orderId: order._id,
+      date: formatDate(order.createdAt),
+      status: order.status,
+      products: order.products
+    }
+  }
+
+  useEffect(() => {
+    const newRows = orders.map((ord) => createData(ord))
+    setRows(newRows)
+  }, [orders])
+  console.log(rows)
+  return (
+    <Container>
+      <TableContainer component={Paper}>
+        <Table aria-label="collapsible table">
+          <TableHead>
+            <TableRow>
+              <TableCell />
+              <TableCell>Pedido</TableCell>
+              <TableCell>Cliente</TableCell>
+              <TableCell>Data do pedido</TableCell>
+              <TableCell>Status</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody className="tbory">
+            {rows.map((row) => (
+              <Row key={row.orderId} row={row} />
+            ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
+    </Container>
+  )
+}
+export default Orders
