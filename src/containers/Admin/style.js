08956fc672f2a9828317fb3aaeb6f96bfa574fd2
hercules1997 <hercules.chaves.andrade@gmail.com>
2023-04-13 22:33:27 -0300

    Update do style admin

--- src/containers/Admin/style.js
+++ src/containers/Admin/style.js
@@ -1,4 +1,12 @@
 import styled from 'styled-components'
 
 export const Container = styled.div`
+  display: flex;
+  
+background-color: black;
+
+`
+export const ContainerItems = styled.div`
+  padding: 80px 80px 80px 150px;
+  width: 100%;
 `
