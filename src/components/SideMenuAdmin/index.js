cf1eb290335a856ff235056e9a864d52e376e139
hercules1997 <hercules.chaves.andrade@gmail.com>
2023-04-13 22:31:25 -0300

    Estrutura do SideMenuAdmin

---
+++ src/components/SideMenuAdmin/index.js
@@ -0,0 +1,48 @@
+import { LogoutOutlined } from '@mui/icons-material'
+import PropTypes from 'prop-types'
+import React from 'react'
+import { useHistory } from 'react-router-dom'
+
+import { useUser } from '../../hooks/UserContext'
+import listLinks from './menu-list'
+import {
+  ContainerItems,
+  ContainerMenu,
+  ListLink,
+  ContainerLogout
+} from './style'
+
+export function SideMenuAdmin ({ path }) {
+  const { logout } = useUser()
+  const { push } = useHistory()
+
+  return (
+    <ContainerMenu>
+      <hr className="hr"></hr>
+      {listLinks.map((item) => (
+        <ContainerItems key={item.id}>
+          <ListLink to={item.link} isActive={path === item.link}>
+            <item.icon className="icon" to={item.link} />
+            {item.label}
+          </ListLink>
+        </ContainerItems>
+      ))}
+      <hr></hr>
+      <ContainerLogout
+        onClick={() => {
+          logout()
+          push('/login')
+        }}
+      >
+        <ListLink style={{ color: '#fa9600' }}>
+          <LogoutOutlined className="iconLogout" />
+          Sair
+        </ListLink>
+      </ContainerLogout>
+    </ContainerMenu>
+  )
+}
+
+SideMenuAdmin.propTypes = {
+  path: PropTypes.string
+}
