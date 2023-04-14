e48a79e7f7e7f7154820271a121dd4f64e05d2dc
hercules1997 <hercules.chaves.andrade@gmail.com>
2023-04-13 22:26:39 -0300

    Criando o formato de data e hora

--- src/utils/formatDate.js
+++ src/utils/formatDate.js
@@ -1,5 +1,11 @@
 const formatDate = (date) => {
-  return new Date(date).toLocaleDateString()
+  return new Date(date).toLocaleDateString('pt-BR', {
+    yarn: '2-digit',
+    day: '2-digit',
+    month: 'short',
+    hour: '2-digit',
+    minute: '2-digit'
+  })
 }
 
 export default formatDate
