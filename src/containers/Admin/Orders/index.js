cafb9c0ff0161525ce55f733866764949625fe74
hercules1997 <hercules.chaves.andrade@gmail.com>
2023-04-13 22:36:19 -0300

    criando a tabela do status dos produtos

--- src/containers/Admin/Orders/index.js
+++ src/containers/Admin/Orders/index.js
@@ -9,19 +9,22 @@ import React, { useEffect, useState } from 'react'
 
 import apiTopBurger from '../../../services/api'
 import formatDate from '../../../utils/formatDate'
+import status from './order-startus'
 import Row from './row'
-import { Container } from './style'
+import { Container, Menu, LinkMenu } from './style'
 
 function Orders () {
   const [orders, setOrders] = useState([])
+  const [filteredOrders, setFilteredOrders] = useState([])
+  const [activeStatus, setActiveStatus] = useState(0)
   const [rows, setRows] = useState([])
 
-  console.log(orders)
   useEffect(() => {
     async function loadOrders () {
       const { data } = await apiTopBurger.get('orders')
 
       setOrders(data)
+      setFilteredOrders(data)
     }
 
     loadOrders()
@@ -38,12 +41,47 @@ function Orders () {
   }
 
   useEffect(() => {
-    const newRows = orders.map((ord) => createData(ord))
+    const newRows = filteredOrders.map((ord) => createData(ord))
     setRows(newRows)
+  }, [filteredOrders])
+
+  useEffect(() => {
+    if (activeStatus === 0) {
+      setFilteredOrders(orders)
+    } else {
+      const statusIndex = status.findIndex((sts) => sts.id === activeStatus)
+      const newFilterOrders = orders.filter(
+        (ord) => ord.status === status[statusIndex].value
+      )
+      setFilteredOrders(newFilterOrders)
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [orders])
-  console.log(rows)
+
+  function headleStatus (status) {
+    if (status.id === 0) {
+      setFilteredOrders(orders)
+    } else {
+      const newOrder = orders.filter((ord) => ord.status === status.value)
+      setFilteredOrders(newOrder)
+    }
+    setActiveStatus(status.id)
+  }
+
   return (
     <Container>
+      <Menu>
+        {status &&
+          status.map((status) => (
+            <LinkMenu
+              key={status.id}
+              onClick={() => headleStatus(status)}
+              isActiveStatus={activeStatus === status.id}
+            >
+              {status.label}
+            </LinkMenu>
+          ))}
+      </Menu>
       <TableContainer component={Paper}>
         <Table aria-label="collapsible table">
           <TableHead>
@@ -57,7 +95,12 @@ function Orders () {
           </TableHead>
           <TableBody className="tbory">
             {rows.map((row) => (
-              <Row key={row.orderId} row={row} />
+              <Row
+                key={row.orderId}
+                row={row}
+                setOrders={setOrders}
+                orders={orders}
+              />
             ))}
           </TableBody>
         </Table>
