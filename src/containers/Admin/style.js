0feec3e5123c12bf51b2cf375c60fbc861fbe4ba
hercules1997 <hercules.chaves.andrade@gmail.com>
2023-04-21 20:25:18 -0300

    removendo o min-heigth do container

--- src/containers/Admin/style.js
+++ src/containers/Admin/style.js
@@ -2,11 +2,11 @@ import styled from 'styled-components'
 
 export const Container = styled.div`
   display: flex;
-  
-background-color: black;
-
+  min-height: 100vh;
+  background-color: black;
 `
 export const ContainerItems = styled.div`
   padding: 80px 80px 80px 150px;
-  width: 100%;
+  width: calc(100vw - 200px);
+  margin-left: 200px;
 `
