b13983ff713a9c6fe6cae419321414cd6fd1e3a6
hercules1997 <hercules.chaves.andrade@gmail.com>
2023-04-21 20:29:26 -0300

    reconfigurando os styles do newProduct

--- src/containers/Admin/NewProducts/style.js
+++ src/containers/Admin/NewProducts/style.js
@@ -1,26 +1,29 @@
+import ReactSelect from 'react-select'
 import styled from 'styled-components'
 
 import { Button } from '../../../components/Button'
 
 export const Container = styled.div`
-  color: white;
-  width: 100%;
-  height: 100%;
+  /* min-height: 100vh; */
   display: flex;
   justify-content: center;
   align-items: center;
 
   form {
+    display: flex;
+    flex-direction: column;
     background: #2a2a2aba;
-    padding: 30px;
+    padding: 35px;
+    justify-content: space-around;
     border-radius: 18px;
-
-    width: 350px;
+    color: white;
+    width: 700px;
+    height: 500px;
+    gap: 20px;
   }
 `
 export const Label = styled.p`
   font-size: 1rem;
-  margin-bottom: 5px;
 `
 
 export const Input = styled.input`
@@ -30,28 +33,47 @@ export const Input = styled.input`
   color: white;
   font-size: 17px;
   padding: 12px;
+  outline: none;
   border-radius: 10px;
   border: none;
-  margin-bottom: 25px;
+  border: ${(props) => (props.error ? '2px solid yellow' : 'none')};
 `
 export const ButtonStyle = styled(Button)`
   font-size: 16px;
   padding: 10px;
   width: 100%;
+  margin-top: 0;
 `
 export const LabelUpload = styled.label`
   cursor: pointer;
   display: flex;
   align-items: center;
-  border: dashed 4px black;
+  border: ${(props) => (props.error ? '3px solid yellow' : 'dashed 3px black')};
   border-radius: 12px;
-  padding: 10px;
+  padding: 8px;
   gap: 3px;
   text-align: center;
   justify-content: center;
-  margin-bottom: 25px;
+
   input {
     opacity: 0;
     width: 1px;
+  }
+`
+export const ReactSelectStyles = styled(ReactSelect)`
+  color: black;
+    background: black !important;
+
+  .css-10wo9uf-option {
+    background: black !important;
+    outline: none;
+
+    color: white;
+
+
+  }
+
+  .css-13cymwt-control {
+    background: black;
   }
 `
