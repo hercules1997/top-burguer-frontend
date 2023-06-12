5ff3450a88dffca6764e8438e1c5723d2d561335
hercules1997 <hercules.chaves.andrade@gmail.com>
2023-06-12 12:52:53 -0300

    Criando o isloading

--- src/containers/Home/index.js
+++ src/containers/Home/index.js
@@ -1,46 +1,38 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
 import ImgHomeBurguer from '../../assets/ImgHomeBurguer.png'
 import { CategoryCarousel, OffersCarousel } from '../../components'
-import { Container, ContainerItens } from './style'
+import { Container, ContainerItens, Loading } from './style'
 
 export function Home () {
-  // const [isLoading, setIsLoading] = useState(true)
+  const [isLoading, setIsLoading] = useState(true)
 
   // Simulando um atraso de 2 segundos para a demonstração
-  // useEffect(() => {
-  //   setTimeout(() => {
-  //     setIsLoading(false)
-  //   }, 1500)
-  // }, [])
+  useEffect(() => {
+    setTimeout(() => {
+      setIsLoading(false)
+    }, 2000)
+  }, [])
 
   return (
-    <Container>
-      <img src={ImgHomeBurguer} />
+    <>
+      {isLoading
+        ? (
+        <Loading>
+          <span>Buscando produtos...</span>
+        </Loading>
+          )
+        : (
+        <></>
+          )}
+      <Container>
+        <img src={ImgHomeBurguer} />
 
-      <ContainerItens>
-        <OffersCarousel />
-        <CategoryCarousel />
-      </ContainerItens>
-    </Container>
+        <ContainerItens>
+          <OffersCarousel />
+          <CategoryCarousel />
+        </ContainerItens>
+      </Container>
+    </>
   )
 }
-
-// <>
-//   {isLoading
-//     ? (
-//     <Loading>
-//       <span>...</span>
-//     </Loading>
-//       )
-//     : (
-//     <Container>
-//       <img src={ImgHomeBurguer} />
-
-//       <ContainerItens>
-//         <OffersCarousel />
-//         <CategoryCarousel />
-//       </ContainerItens>
-//     </Container>
-//       )}
-// </>
