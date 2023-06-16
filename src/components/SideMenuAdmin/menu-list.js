063662ea3c50ab11997539cb01a8a30223a1c48e
hercules1997 <hercules.chaves.andrade@gmail.com>
2023-06-16 17:40:03 -0300

    Add edição de categorias

--- src/components/SideMenuAdmin/menu-list.js
+++ src/components/SideMenuAdmin/menu-list.js
@@ -1,6 +1,6 @@
 import { ShoppingBag } from '@mui/icons-material'
 import AddCircleIcon from '@mui/icons-material/AddCircle'
-// import CategoryIcon from '@mui/icons-material/Category'
+import CategoryIcon from '@mui/icons-material/Category'
 import HomeIcon from '@mui/icons-material/Home'
 import ListAltIcon from '@mui/icons-material/ListAlt'
 
@@ -29,14 +29,14 @@ const listLinks = [
     label: 'Novo Produto',
     link: paths.NewProducts,
     icon: AddCircleIcon
-  }
+  },
 
-  // {
-  //   id: 5,
-  //   label: 'Categorias',
-  //   link: paths.NewCategory,
-  //   icon: CategoryIcon
-  // }
+  {
+    id: 5,
+    label: 'Categorias',
+    link: paths.NewCategory,
+    icon: CategoryIcon
+  }
 ]
 
 export default listLinks
